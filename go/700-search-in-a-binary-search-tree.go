0757bf4881f701a9da800cc42bb030a7cfd77901
Qihang Chen <qihang@thumbtack.com>
2020-04-11 21:32:57 -0700

    search bst

---
+++ go/700-search-in-a-binary-search-tree.go
@@ -0,0 +1,14 @@
+package main
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if val == root.Val {
+		return root
+	}
+	if val < root.Val {
+		return searchBST(root.Left, val)
+	}
+	return searchBST(root.Right, val)
+}
