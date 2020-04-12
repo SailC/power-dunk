e56bae04c624729021c52c7502ba6395e03d8719
Qihang Chen <qihang@thumbtack.com>
2020-04-11 21:48:30 -0700

    insert bst

---
+++ go/701-insert-into-a-binary-search-tree.go
@@ -0,0 +1,17 @@
+package main
+
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return &TreeNode{
+			Val:   val,
+			Left:  nil,
+			Right: nil,
+		}
+	}
+	if val < root.Val {
+		root.Left = insertIntoBST(root.Left, val)
+	} else {
+		root.Right = insertIntoBST(root.Right, val)
+	}
+	return root
+}
